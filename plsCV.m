dc4a75b55163a977ee9216f2ff041f5ec8cbc763
Alex Teghipco <alex.teghipco@uci.edu>
2023-06-28 14:05:36 -0400

    Update plsCV.m

--- plsCV.m
+++ plsCV.m
@@ -1,7 +1,34 @@
 function [pls_yh,yNorm,winC,winCLoss,fk,allBetas] = plsCV(X,y,alpha,numComponentsRange,cvo,cvi,standardizeX,standardizeY,parallel,feats,ssVals)
-tmpsave = false;
-saveBetas = true;
-impute = true;
+% plsCV performs partial least squares regression in a nested CV scheme. 
+% You must provide your own outer and inner cvpartitions. This will also
+% interface with stability selection to run independent PLS with VIP selection
+% on 50 complementary bootstraps of the training datasets 
+% (outer folds; using 50% of the data). StabSel will be run across selected dimensionalities 
+%
+% INPUTS -----
+% X: n x p matrix of n samples and p predictors
+% y: n x r matrix of n samples and r response variables to predict
+% alpha: weighting of correlation vs MSE (0 is all correlation, 1 is all MSE)
+% numComponentsRange: full set of # of components to test in inner loop
+% cvo: 1 x o cell of cvpartition objects for outer folds. If > 1 this amounts to repeats of CV
+% cvi: o x i cell of cvpartition objects for inner folds. i reflects the inner folds per o (i.e., outer fold)
+% standardizeX: true or false to do standardization of X or not (columnwise z-scoring)
+% standardizeY: true or false to do standardization of y or not (columnwise z-scoring)
+% parallel: if true, pls with use parallelization of cpus
+% feats: set empty to ignore or give cell array where values within each cell correspond to the indices of a specific set of features (columns in X) to use during training
+% ssVals: set empty to ignore or set to a value which will act as the threshold for VIP. Features > this VIP in bootstraps of the data will be "selected" by stability selection
+%
+% OUTPUTS -----
+% pls_yh : predictions of y
+% yNorm : normalized y values if standardization was true
+% winC: winning # of components
+% winCLoss: loss for winning # of components
+% fk: selected features by stability selection with VIP
+% allBetas: betas of outer training fold PLS models
+
+tmpsave = false; % set this to true to save out intermediary data
+saveBetas = true; % if true, betas will be saved
+impute = true; % if data is missing, impute with KNN by taking data from closest sample based on correlation
 
 %pls_yh = zeros(size(y));
 if isempty(feats)
@@ -12,6 +39,9 @@ doSS = true;
 if isempty(ssVals)
     ssVals = 1;
     doSS = false;
+end
+if doSS
+    warning('Make sure you have my stabSel repository installed!!')
 end
 
 if impute
@@ -59,14 +89,14 @@ for r = 1:length(cvo)
             if parallel
                 if doSS
                     [fktmp{r,k},~,fscmxtmp{r,k},~,~,~,~,~,~,~,~,~,~,~] = stabSel(trXX,trYY,...
-                        'parallel',false,'stnd',false,'compPars',true,'samType','bootstrap','filter',false,'filterThresh',1.5,...
+                        'parallel',false,'stnd',standardizeX,'compPars',true,'samType','bootstrap','filter',false,'filterThresh',1.5,...
                         'maxVars',[],'thresh',max(ssVals),'fdr',[],'numFalsePos',[],'nComp',[min(ncrtmp):1:max(ncrtmp)],'selAlgo',...
                         'pls','rep',50,'prop',0.5,'parallel',true,'verbose',false); %maxVars 50 gives 16
                 end
             else
                 if doSS
                      [fktmp{r,k},~,fscmxtmp{r,k},~,~,~,~,~,~,~,~,~,~,~] = stabSel(trXX,trYY,...
-                        'parallel',false,'stnd',false,'compPars',true,'samType','bootstrap','filter',false,'filterThresh',1.5,...
+                        'parallel',false,'stnd',standardizeX,'compPars',true,'samType','bootstrap','filter',false,'filterThresh',1.5,...
                         'maxVars',[],'thresh',max(ssVals),'fdr',[],'numFalsePos',[],'nComp',[min(ncrtmp):1:max(ncrtmp)],'selAlgo',...
                         'pls','rep',50,'prop',0.5,'parallel',true,'verbose',false); %maxVars 50 gives 16
                 end
